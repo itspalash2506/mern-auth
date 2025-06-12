8f69b127c509d3ace53652d3a02f4598bc719be5
itspalash250604 <palash.ghosh2022@vitstudent.ac.in>
2025-06-12 23:15:42 +0530

    Create Sign-Up API Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ mongoose
   });
 
 const app = express();
-
+app.use(express.json()); //This line allows the server to parse JSON bodies in requests
 app.listen(3000, () => {
   console.log("Server listening on port 3000! ");
 });
@@ -27,3 +28,4 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
