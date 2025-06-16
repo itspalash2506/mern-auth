102afe59b5f28c8ccecc14e14679add464f2d6cf
Palash <palashghoshdastidar@gmail.com>
2025-06-17 01:33:16 +0530

    Create Header Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return <BrowserRouter>
   <Header/>
   <Routes>
     <Route path="/" element={<Home/>}>home</Route>
-    <Route path="/about" element={<Home/>}>about</Route>
-    <Route path="/profile" element={<Home/>}>profile</Route>
-    <Route path="/sign-in" element={<Home/>}>sign-in</Route>
-    <Route path="/sign-up" element={<Home/>}>sign-up</Route>
+    <Route path="/about" element={<About/>}>about</Route>
+    <Route path="/profile" element={<Profile/>}>profile</Route>
+    <Route path="/sign-in" element={<SignIn/>}>sign-in</Route>
+    <Route path="/sign-up" element={<SignUp/>}>sign-up</Route>
   </Routes>
   </BrowserRouter>;
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="bg-slate-300">
+      <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <Link to="/">
+          <h1 className="font-bold">Auth App</h1>
+        </Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li>Sing-In</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
